f72b04c086ce287c55b55a9b419077ec460f1c81
Sir_Floppa <nicolas.donoso.ge@outlook.com>
2021-04-24 12:19:50 -0400

    Prepared to add idle animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
     public Animator anim;
 
     public int speed = 200;
+    public string lastDirection;
     public Vector3 direction;
 
     // Makes the player w a l k
@@ -16,23 +17,42 @@ public class PlayerMovement : MonoBehaviour
         // Defines the animation of the character in function of his movement
         if (direction.x > 0)
         {
-            /// Set the walking right animation            
+            /// Set the walking right animation        
+            lastDirection = "Right";
         }
         else if (direction.x < 0)
         {
             /// Set the walking left animation
+            lastDirection = "Left";
         }
         else if (direction.y > 0)
         {
             /// Set the walking up animation
+            lastDirection = "Up";
         }
         else if (direction.y < 0)
         {
             /// Set the walking down animation
+            lastDirection = "Down";
         }
         else
         {
-            /// Set the idle animation
+            // Set the idle animation
+            switch (lastDirection)
+            {
+                case "Right":
+                    /// Set idle right animation
+                    break;
+                case "Left":
+                    /// Set idle left animation
+                    break;
+                case "Up":
+                    /// Set idle right animation
+                    break;
+                case "Down":
+                    /// Set idle left animation
+                    break;
+            }
         }
     }
 }
