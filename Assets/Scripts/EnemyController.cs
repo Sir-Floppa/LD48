83b21abdb3cd21669fde2731c0ea9f5b56f2bba8
Sir_Floppa <nicolas.donoso.ge@outlook.com>
2021-04-25 16:13:16 -0400

    Commented the enemy script

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -6,6 +6,7 @@ public class EnemyController : MonoBehaviour
 {
     public GameObject target;
     public GameObject player;
+    public Animator anim;
     public float speed = 2.25f;
 
     // Start is called before the first frame update
@@ -17,11 +18,13 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Follow the player and sets the moving animation for the enemy
         if (target != null)
         {
             transform.Translate((target.transform.position - transform.position) * speed * Time.deltaTime);
         }
 
+        // Sets the order in layer in function of the player's position
         if (player.transform.position.y < transform.position.y)
         {
             GetComponent<SpriteRenderer>().sortingOrder = player.GetComponent<SpriteRenderer>().sortingOrder - 1;
@@ -42,6 +45,7 @@ public class EnemyController : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D collision)
     {
+        // Finds the player and targets him
         if (collision.gameObject.tag == "Player")
         {
             target = null;
