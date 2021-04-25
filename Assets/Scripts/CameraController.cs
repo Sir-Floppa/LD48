97f1e88bbbfeaee857db86272af2d41f063db501
Jose Pereira <wallbangstudios4.9@gmail.com>
2021-04-25 14:09:55 -0400

    RoomTest Scene, sprite Changes, cameraScript

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public GameObject player;
-    public float speed = 0.025f;
+    //public float speed = 0.025f;
+
+    public float smoothTime = .50f;
     public Vector3 offset;
+
+
+
+    Vector3 velocity = Vector3.zero;
 
     private void LateUpdate()
     {
         Vector3 desiredPosition = player.transform.position + offset;
-        Vector3 smoothPosition = Vector3.Lerp(transform.position, desiredPosition, speed);
+        Vector3 smoothPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothTime);
         smoothPosition.z = -10;
         transform.position = smoothPosition;
     }
